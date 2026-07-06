938974e0178c81433201f28a781db5bea6c24ce8
ey9gh0 <ey9github0@gmail.com>
2026-07-06 13:39:33 +0700

    Update booking.py

--- booking.py
+++ booking.py
@@ -1,25 +1,20 @@
 from playwright.sync_api import sync_playwright
 
-URL="https://script.google.com/a/macros/banksinarmas.com/s/AKfycbyGVQZaMoU4Q4HOS51V2Tmt_nnO2UNu4QCfUbk6EWuGVYtamrhMMLoUv-kI1oGHU9-0Nw/exec?v=bookWorkSite"
+URL = "https://script.google.com/a/macros/banksinarmas.com/s/AKfycbyGVQZaMoU4Q4HOS51V2Tmt_nnO2UNu4QCfUbk6EWuGVYtamrhMMLoUv-kI1oGHU9-0Nw/exec?v=bookWorkSite"
 
 with sync_playwright() as p:
-
     browser = p.chromium.launch(headless=True)
 
     page = browser.new_page()
 
-    page.goto(URL)
-
-    page.wait_for_timeout(5000)
+    page.goto(URL, wait_until="networkidle")
 
     frame = page.frames[2]
 
-    print("===== WORKSITE =====")
-
+    print("========== WORKSITE ==========")
     print(frame.locator("#workSite").inner_html())
 
-    print("===== DATE =====")
-
-    print(frame.locator("#meetingDate").input_value())
+    print("========== MEETING DATE ==========")
+    print(frame.locator("#meetingDate").evaluate("e => e.outerHTML"))
 
     browser.close()
