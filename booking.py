26f843caa65929dbcc0393c8837073767ecd6cbf
ey9gh0 <ey9github0@gmail.com>
2026-07-06 13:36:45 +0700

    Update booking.py

--- booking.py
+++ booking.py
@@ -1,54 +1,25 @@
 from playwright.sync_api import sync_playwright
-import os
-import sys
 
-URL = "https://script.google.com/a/macros/banksinarmas.com/s/AKfycbyGVQZaMoU4Q4HOS51V2Tmt_nnO2UNu4QCfUbk6EWuGVYtamrhMMLoUv-kI1oGHU9-0Nw/exec?v=bookWorkSite"
+URL="https://script.google.com/a/macros/banksinarmas.com/s/AKfycbyGVQZaMoU4Q4HOS51V2Tmt_nnO2UNu4QCfUbk6EWuGVYtamrhMMLoUv-kI1oGHU9-0Nw/exec?v=bookWorkSite"
 
-def main():
-    with sync_playwright() as p:
-        browser = p.chromium.launch(
-            headless=True
-        )
+with sync_playwright() as p:
 
-        page = browser.new_page(
-            viewport={"width": 1920, "height": 1080}
-        )
+    browser = p.chromium.launch(headless=True)
 
-        print("Opening website...")
-        page.goto(URL, wait_until="networkidle", timeout=120000)
+    page = browser.new_page()
 
-        page.screenshot(path="page.png", full_page=True)
+    page.goto(URL)
 
-        print("=" * 50)
-        print("TITLE :", page.title())
-        print("URL   :", page.url)
-        print("=" * 50)
+    page.wait_for_timeout(5000)
 
-        frames = page.frames
+    frame = page.frames[2]
 
-        print(f"Total Frames : {len(frames)}")
+    print("===== WORKSITE =====")
 
-        for i, frame in enumerate(frames):
-            print("-" * 50)
-            print(f"Frame {i}")
-            print(frame.url)
+    print(frame.locator("#workSite").inner_html())
 
-            try:
-                ids = frame.locator("[id]").evaluate_all(
-                    """els => els.map(e => e.id)"""
-                )
+    print("===== DATE =====")
 
-                print(ids)
+    print(frame.locator("#meetingDate").input_value())
 
-            except Exception as e:
-                print(e)
-
-        browser.close()
-
-
-if __name__ == "__main__":
-    try:
-        main()
-    except Exception as e:
-        print(e)
-        sys.exit(1)
+    browser.close()
